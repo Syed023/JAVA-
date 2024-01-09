0ece14f508e4b0601d6c19e4d5c639718fd81e6b
Syed023 <152625426+Syed023@users.noreply.github.com>
2024-01-10 02:15:04 +0530

    Create AOC.java

---
+++ AOC.java
@@ -0,0 +1,9 @@
+class AOC{
+public static void main(String[] args) {
+double radius = 7.5;
+double perimeter = 2 * 3.141 * radius;
+double area = 3.141* radius * radius;
+System.out.println("Perimeter is = " + perimeter);
+System.out.println("Area is = " + area);
+}
+}
